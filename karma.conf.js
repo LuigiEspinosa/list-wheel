3d9400ac874db784fad8b4ab30cd25844d12d006
LuigiEspinosa <luigi@cuatro.dev>
2026-02-23 01:54:42 -0500

    chore(test): Full karma setup

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,34 @@
 module.exports = function (config) {
   config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+    ],
+    client: {
+      jasmine: {
+        random: false,
+      },
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true,
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/list-wheel'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    reporters: ['progress', 'kjhtml'],
     browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-setuid=sandbox'],
+        flags: ['--no-sandbox', '--disable-setuid-sandbox'],
       },
     },
+    restartOnFileChange: true,
   });
 };
